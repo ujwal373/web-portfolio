e43790398cf8fe9830dc616ae9ab1db1e05e2dc8
ujwal373 <ujwal.mojidra@gmail.com>
2026-04-29 20:31:26 +0100

    DEL: old files

--- neural-portfolio/src/lib/utils/cn.ts
+++
@@ -1,9 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-/**
- * Utility function to merge Tailwind CSS classes
- */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
